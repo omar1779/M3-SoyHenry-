d6a6f37084000dc557f8182ddfc404501b690a5c
omar1779 <josemyclans@gmail.com>
2022-07-01 00:05:54 -0500

    homework terminada

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,16 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+http.createServer((request , response) =>{
+    fs.readFile(`${__dirname}/images/${request.url}.jpg`,
+    (err , data) =>{
+        if(err){
+            response.writeHead(404, {'content-type': 'text/plain'})
+            response.end('Hubo un error ,not found')
+        }
+        else{
+            response.writeHead(200 , {'content-type':'image/jpg'})
+            response.end(data)
+        }
+    })
+}).listen(3000, '127.0.0.1')
